f6aaf9f445741ce5e318552ab1ac2c83dc116f07
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-08 16:00:30 +0300

    Update word_counts.py

--- word_count_lab/word_counts.py
+++ word_count_lab/word_counts.py
@@ -1,17 +1,6 @@
 def word_counts(word):
   """ Check the number of times an element appears in a word"""
-    repeat = []
-    length = len(word)
-    count =0
-    counter =[]
     words = {}
-    for l in range(0,length):
-        for c in range(l +1, length):
-            if word[l] == word[c]:
-                repeat.append(word[l])
-                print(repeat)
-            
-                count = count +1
-                counter.append(count)
-            count +=1
-    print(counter)
+    for i in (word.split()):
+        
+
