36ea3eba4e115ffe5c4853186feb77c9400a3de6
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-08 11:15:41 +0300

    Modified and working find_max_min.py

--- max_and_min_lab/find_max_min.py
+++ max_and_min_lab/find_max_min.py
@@ -3,9 +3,15 @@ def find_max_min(num_list):
 	""" find the minimum and maximum numbers in a list and return them in an Array in the format [min, max]"""
 
 	my_list =[]
+	new_list+[]
 
 	maximum = max(num_list)
 	minimum = min(num_list)
 
-	my_list.append([minimum, maximum])
+	if minimum == maximum:
+		new_list.append(len(num_list))
+		return new_list
+
+	my_list.append(minimum)
+	my_list.append(maximum)
 	return my_list
