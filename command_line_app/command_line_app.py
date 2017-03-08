41fc7c067711eb279d29036c53511b3d2961643b
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-08 17:15:56 +0300

    minor tweaks to the command_line_app.py

--- command_line_app/command_line_app.py
+++ command_line_app/command_line_app.py
@@ -1,16 +1,23 @@
 """discovery.py is the build() function needed to create a service endpoint
 for interacting with an API"""
 "This application let's you query public ativities in the Google+ API HTTP Client Library "
+import httplib2
+import sys
 from apiclient.discovery import build
-
 
 API_KEY = 26b7fc1faccc9bccc4714f78ea9db6ac11e3015b
 SERVICE = build(API, VERSION, developerKey=API_KEY)
 
+TMPL = '''
+    User: %s
+    Date: %s
+    Post: %s
+'''
+
 GPLUS = discovery.build('plus', 'v1', developerKey=API_KEY)
 
 "Use search() method to query public activities"
-items = GPLUS.activities().search(query='python').execute().get('items', [])
+items = GPLUS.activities().search(query='heaven').execute().get('items', [])
 for data in items:
 	post = ' '.join(data['title'].strip().split())
 	if post:
