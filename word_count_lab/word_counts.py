402df3d14ce61ab474952fe7086597bf0e0251ad
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-08 16:42:50 +0300

    Update word_counts.py

--- word_count_lab/word_counts.py
+++ word_count_lab/word_counts.py
@@ -1,6 +1,15 @@
-def word_counts(word):
-  """ Check the number of times an element appears in a word"""
-    words = {}
-    for i in (word.split()):
+from collections import Counter
+
+def words(word):
+    """ Check the number of times an element appears in a word"""
+    new_list = []
+    
+    for i in word.split():
+        new_list.append(i)
+    return Counter(new_list)
+
+    
+
         
+
 
