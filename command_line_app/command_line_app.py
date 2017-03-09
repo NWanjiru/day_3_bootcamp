13dbb6476d286e359b033ed7e257a5e4f0d1169f
Nyawira Wanjiru <leenjiru1@gmail.com>
2017-03-09 06:15:01 +0300

    Modified command_line_app.py

--- command_line_app/command_line_app.py
+++ command_line_app/command_line_app.py
@@ -1,25 +1,8 @@
-"""discovery.py is the build() function needed to create a service endpoint
-for interacting with an API"""
-"This application let's you query public ativities in the Google+ API HTTP Client Library "
-import httplib2
-import sys
-from apiclient.discovery import build
+import urllib3 
+import requests
 
-API_KEY = 26b7fc1faccc9bccc4714f78ea9db6ac11e3015b
-SERVICE = build(API, VERSION, developerKey=API_KEY)
+my_google = requests.get("http://raynji.blogspot.co.ke/")
 
-TMPL = '''
-    User: %s
-    Date: %s
-    Post: %s
-'''
-
-GPLUS = discovery.build('plus', 'v1', developerKey=API_KEY)
-
-"Use search() method to query public activities"
-items = GPLUS.activities().search(query='heaven').execute().get('items', [])
-for data in items:
-	post = ' '.join(data['title'].strip().split())
-	if post:
-		print(TMPL % (data['actor']['displayName'],
-						data['published'], post))
+print (my_google.headers)
+print (my_google.text[500:1000])
+print(my_google.status_code)
